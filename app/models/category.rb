0f8ca891c22fc8ca8b4748510ada72f43365009f
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-05-11 20:12:14 +0530

    high level category validations and seed data script

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,30 @@
 class Category < ApplicationRecord
+  include Slugable
+
+  source_for_slug :name
+
+  enum eligible_mode: {
+      creditable: 0,
+      debitable: 1
+  }
+
+  # associations
   belongs_to :user
   has_many :expense_logs
+
+  # validations
+  before_validation :generate_slug, if: :name_changed?
+
+  validates_presence_of :name
+  validates_presence_of :user
+  validates_presence_of :slug
+  validates_presence_of :eligible_mode
+
+  # scopes
+
+  # callbacks
+
+  # instance methods
+
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,75 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@gringotts.com', password: 'get_rich_or_die_codin', password_confirmation: 'get_rich_or_die_codin') if Rails.env.development?
+
+def create_seed_users
+  User.create!([
+                   {
+                       email: 'admin@gringotts.com',
+                       password: "get_rich_or_die_codin"
+                   },
+                   {
+                       email: 'demo@gringotts.com',
+                       password: "demodemodemodemodemodemodemo"
+                   }
+               ])
+end
+
+def create_creditable_categories
+
+  admin_user = User.find_by_email('admin@gringotts.com')
+  return if admin_user.blank?
+
+  Category.create!([
+                       {
+                           name: 'Salary',
+                           user: admin_user,
+                           eligible_mode: :creditable
+                       },
+                       {
+                           name: 'Gift',
+                           user: admin_user,
+                           eligible_mode: :creditable
+                       },
+                       {
+                           name: 'Bonus',
+                           user: admin_user,
+                           eligible_mode: :creditable
+                       }
+                   ])
+end
+
+def create_debitable_categories
+  admin_user = User.find_by_email('admin@gringotts.com')
+  return if admin_user.blank?
+
+  Category.create!([
+                       {
+                           name: 'Commute',
+                           user: admin_user,
+                           eligible_mode: :debitable
+                       },
+                       {
+                           name: 'Food',
+                           user: admin_user,
+                           eligible_mode: :debitable
+                       },
+                       {
+                           name: 'Recreation',
+                           user: admin_user,
+                           eligible_mode: :debitable
+                       }
+                   ])
+
+end
+
+if Rails.env.development?
+
+  ActiveRecord::Base.transaction do
+    create_seed_users
+    create_creditable_categories
+    create_debitable_categories
+  end
+
+end
+
