e0bcefea2f29b194800f0b25937415a09af9ae2a
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-10-07 00:29:47 +0530

    uncommenting test case

--- spec/models/account.rb
+++ spec/models/account.rb
@@ -31,33 +31,33 @@ RSpec.describe Account, type: :model do
     it { should have_many(:favorites) }
   end
 
-  # context "with two accounts" do
-  #   it "create two accounts" do
-  #
-  #     admin_user = create(:admin_user)
-  #     demo_user = create(:demo_user)
-  #
-  #     account_1 = {
-  #         "name": "Acct 1",
-  #         "user_id": admin_user.id,
-  #         "current_value": 2000
-  #     }
-  #
-  #     account_2 = account_1.dup
-  #     account_2["name"] = "Acct 2"
-  #
-  #     Account.create! account_1
-  #     Account.create! account_2
-  #
-  #     all_accounts = Account.all
-  #     expect(all_accounts.size).to eq(2)
-  #
-  #     account_names = all_accounts.map(&:name)
-  #
-  #     expect(account_names).to eq(["Acct 1", "Acct 2"])
-  #   end
-  #
-  # end
+  context "with two accounts" do
+    it "create two accounts" do
+
+      admin_user = create(:admin_user)
+      demo_user = create(:demo_user)
+
+      account_1 = {
+          "name": "Acct 1",
+          "user_id": admin_user.id,
+          "current_value": 2000
+      }
+
+      account_2 = account_1.dup
+      account_2["name"] = "Acct 2"
+
+      Account.create! account_1
+      Account.create! account_2
+
+      all_accounts = Account.all
+      expect(all_accounts.size).to eq(2)
+
+      account_names = all_accounts.map(&:name)
+
+      expect(account_names).to eq(["Acct 1", "Acct 2"])
+    end
+
+  end
 
 
   context "for Frozen Concern" do
